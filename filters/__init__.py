ad303911df46751628548476e0603f17defb91a4
Turdibek <turdibekdev05@gmail.com>
2022-04-15 20:10:10 +0500

    add filters

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,9 +1,10 @@
 from aiogram import Dispatcher
 
 from loader import dp
-# from .is_admin import AdminFilter
+from .is_private import IsPrivate
+from .is_group import IsGroup
 
 
 if __name__ == "filters":
-    #dp.filters_factory.bind(is_admin)
+    # dp.filters_factory.bind(is_admin)
     pass
