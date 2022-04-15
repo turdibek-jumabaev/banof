c39b012fa6d491d2bfc9e6dbb4b2af43b380dd59
Turdibek <turdibekdev05@gmail.com>
2022-04-15 20:16:14 +0500

    start - command

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -3,8 +3,9 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
 from keyboards.inline import addGroup
+from filters import IsPrivate
 
 
-@dp.message_handler(CommandStart())
+@dp.message_handler(CommandStart(), IsPrivate())
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}! Banof ishlashi uchun guruhga qo'shing.", reply_markup=addGroup)
