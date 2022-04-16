2a3da38f777e91eba1952b1998f53eca01574623
Turdibek <turdibekdev05@gmail.com>
2022-04-16 09:51:50 +0500

    formated

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from aiogram import executor
 
 from loader import dp
-import middlewares, filters, handlers
+import middlewares
+import filters
+import handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
