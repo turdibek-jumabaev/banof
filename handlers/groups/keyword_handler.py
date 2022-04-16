d0dd16fa9da766d588d7c12a40e4051d633feeb0
Turdibek <turdibekdev05@gmail.com>
2022-04-16 10:18:31 +0500

    update groups handler

--- handlers/groups/keyword_handler.py
+++ handlers/groups/keyword_handler.py
@@ -1,14 +1,21 @@
 from loader import dp
+from filters import IsGroup
 
 from aiogram import types
 from time import sleep
 
 
-@dp.message_handler(commands=['ban', 'banof', 'kick', 'kickof', 'banofuz'])
+@dp.message_handler(IsGroup(), commands=['ban', 'banof', 'kick', 'kickof', 'banofuz'])
 async def ban_handler(message: types.Message):
     if message.reply_to_message:
-        matn = f"<b><a href='tg://user?id={message.from_user.id}'>{message.from_user.full_name}</a></b> guruhimiz azosi bo'lgan <b> <a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.full_name}</a></b> foydalanuvchi ban qilmoqchi. \n\n<b>Rozimisiz?</b>\n\n<b>"
-        await message.answer(matn)
+        check = await message.chat.get_member(message.from_user.id)
+        if check.status == 'creator' or check.status == 'administrator':
+            await message.chat.kick(message.reply_to_message.from_user.id, until_date=None)
+            await message.answer(f"<a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a> kick qilindi.")
+            await message.delete()
+        else:
+            matn = f"<b><a href='tg://user?id={message.from_user.id}'>{message.from_user.full_name}</a></b> guruhimiz azosi bo'lgan <b> <a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.full_name}</a></b> foydalanuvchi ban qilmoqchi. \n\n<b>Rozimisiz?</b>\n\n<b>"
+            await message.answer(matn)
     else:
         msg = message.answer("xabarga javob bering")
         await message.delete()
